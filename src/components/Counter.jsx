987de0fbd16e674248d90c880d7cc0b578133345
Mikhail Melnikov <mihail3197@icloud.com>
2024-04-09 15:58:51 -0700

    practice of redux

---
+++ src/components/Counter.jsx
@@ -0,0 +1,32 @@
+import { connect } from "react-redux"
+import {inc, dec, rnd} from "./../actions"
+
+const Counter = ({counter, inc, dec, rnd}) => {
+    return (
+        <div className="jumbotron">
+            <h1>{counter}</h1>
+            <button onClick={dec} className="btn btn-primary">DEC</button>
+            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={inc} className="btn btn-primary">INC</button>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state.value
+        
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        inc: () => dispatch(inc()),
+        dec: () => dispatch(dec()),
+        rnd: () => {
+            const value = Math.floor(Math.random() * 10);
+            dispatch(rnd(value))
+    }
+}}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createStore } from "redux";
+import reducer from "./reducer";
+import App from "./components/App";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = createStore(reducer);
+
+// const { dispatch, subscribe, getState } = store;
+const root = createRoot(document.getElementById("root"));
+// const { inc, rnd, dec } = bindActionCreators(actions, dispatch);
+
+  root.render(
+    <React.StrictMode>
+      {/*<Counter
+        counter={getState().value}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+          const value = Math.floor(Math.random() * 10);
+        rnd(value)}
+        />}*/}
+
+      <Provider store = {store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
